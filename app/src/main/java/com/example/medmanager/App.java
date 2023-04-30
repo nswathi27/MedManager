bb63ce8a3dba65706b451332d7a1188466b1f532
Swathi_Nuli_S555089 <88883122+nswathi27@users.noreply.github.com>
2023-04-29 23:54:10 -0500

    Update App.java

--- app/src/main/java/com/example/medmanager/App.java
+++ app/src/main/java/com/example/medmanager/App.java
@@ -1,43 +1,4 @@
-<<<<<<< HEAD
-package com.example.medmanager;
-
-import android.app.Application;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
-
-public class App extends Application {
-  public static final String CHANNEL_ID = "channel";
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-
-    //createNotificationChannels();
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      NotificationChannel channel = new NotificationChannel(
-              CHANNEL_ID,
-              "MedManager notifications",
-              NotificationManager.IMPORTANCE_HIGH
-      );
-      channel.setDescription("Medicine Reminder Channel");
-      NotificationManager manager = getSystemService(NotificationManager.class);
-      manager.createNotificationChannel(channel);
-    }
-  }
-
-//  private void createNotificationChannels() {
-//    if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-//      NotificationChannel channel = new NotificationChannel(CHANNEL_ID,"MedManager notifications", NotificationManager.IMPORTANCE_HIGH);
-//      channel.setDescription("Medmanager notifications appear here");
-//
-//      NotificationManager manager = getSystemService(NotificationManager.class);
-//      manager.createNotificationChannel(channel);
-//    }
-//  }
-}
-=======
-package com.example.medmanager;
+package com.Project.medmanager;
 
 import android.app.Application;
 import android.app.NotificationChannel;
@@ -64,4 +25,3 @@ public class App extends Application {
     }
   }
 }
->>>>>>> e1554933036cee1d61654db8f1740e6932a7dbd1
