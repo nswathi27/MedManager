ef0f6f6935235f305774d5f7224ad5e7f59b5268
Swathi_Nuli_S555089 <88883122+nswathi27@users.noreply.github.com>
2023-04-30 00:01:15 -0500

    Update MedicalDB.java

--- app/src/main/java/com/example/medmanager/mydatabase/MedicalDB.java
+++ app/src/main/java/com/example/medmanager/mydatabase/MedicalDB.java
@@ -1,4 +1,4 @@
-package com.example.medmanager.mydatabase;
+package com.Project.medmanager.mydatabase;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -14,28 +14,17 @@ public class MedicalDB extends SQLiteOpenHelper {
         if (sInstance == null) {
             sInstance = new MedicalDB(context.getApplicationContext());
         }
-<<<<<<< HEAD
         return sInstance;
     }
 
     public static final int version = 1;
     public MedicalDB(Context context) {
-        super(context,context.getExternalFilesDir(null).getAbsolutePath() + "/database.db", null, version);
-=======
-        
-        return sInstance;
-    }
-    
-    public static final int version = 1;
-    public MedicalDB(Context context) {
         super(context,"database",null, version);
->>>>>>> e1554933036cee1d61654db8f1740e6932a7dbd1
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         String create_user_table = "CREATE TABLE USER (" +
-<<<<<<< HEAD
                 "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "USER_NAME TEXT NOT NULL);";
         String create_med_table = "CREATE TABLE MEDICINE (" +
@@ -48,20 +37,6 @@ public class MedicalDB extends SQLiteOpenHelper {
                 "ENABLE INT NOT NULL,"+
                 "FOREIGN KEY(USER_ID) REFERENCES USER(_id)" +
                 "ON DELETE CASCADE);";
-=======
-                                        "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                                        "USER_NAME TEXT NOT NULL);";
-        String create_med_table = "CREATE TABLE MEDICINE (" +
-                                        "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                                        "MED_NAME TEXT NOT NULL," +
-                                        "QTY INTEGER NOT NULL," +
-                                        "DATE_TIME TEXT NOT NULL," +
-                                        "DAYS TEXT NOT NULL," +
-                                        "USER_ID INT NOT NULL," +
-                                        "ENABLE INT NOT NULL,"+
-                                        "FOREIGN KEY(USER_ID) REFERENCES USER(_id)" +
-                                        "ON DELETE CASCADE);";
->>>>>>> e1554933036cee1d61654db8f1740e6932a7dbd1
         db.execSQL(create_user_table);
         db.execSQL(create_med_table);
     }
@@ -74,48 +49,11 @@ public class MedicalDB extends SQLiteOpenHelper {
     public void onOpen(SQLiteDatabase db) {
         super.onOpen(db);
         if (!db.isReadOnly()) {
-            // Enable foreign key constraints
             db.execSQL("PRAGMA foreign_keys=ON;");
         }
     }
 
-    //    USER CRUD OPERATIONS:
-<<<<<<< HEAD
-    public void addUser(SQLiteDatabase writableDatabase, @NonNull String name)
-    {
-        SQLiteDatabase db = getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put("USER_NAME",name);
-        db.insert("USER",null,values);
-        db.close();
-    }
-    public void deleteUser(SQLiteDatabase writableDatabase, @NonNull String user_id){
-        SQLiteDatabase db = getWritableDatabase();
-        db.delete("USER","_id=?",new String[]{user_id});
-        db.close();
-    }
-    public void updateUser(SQLiteDatabase writableDatabase, @NonNull int user_id, @NonNull String name){
-        SQLiteDatabase db = getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put("USER_NAME",name);
-        db.update("USER",values,"_id=?",new String[]{""+user_id});
-        db.close();
-    }
 
-    public String getUserName(SQLiteDatabase writableDatabase, @NonNull int user_id){
-        SQLiteDatabase db = getReadableDatabase();
-        Cursor user = db.rawQuery("SELECT * FROM USER WHERE _id="+user_id+";",new String[]{});
-        user.moveToFirst();
-        String username = user.getString(1);
-        user.close();
-        db.close();
-        return username;
-    }
-
-    //    MEDICINE CRUD OPERATION:
-    public void addMedicine(SQLiteDatabase writableDatabase, @NonNull int user_id, @NonNull String med_name, @NonNull int quantity, @NonNull String date_time, @NonNull String days){
-        SQLiteDatabase db = getWritableDatabase();
-=======
     public void addUser(SQLiteDatabase db, @NonNull String name)
     {
         ContentValues values = new ContentValues();
@@ -137,9 +75,8 @@ public class MedicalDB extends SQLiteOpenHelper {
         return user.getString(1);
     }
 
-//    MEDICINE CRUD OPERATION:
+
     public void addMedicine(SQLiteDatabase db, @NonNull int user_id, @NonNull String med_name, @NonNull int quantity, @NonNull String date_time, @NonNull String days){
->>>>>>> e1554933036cee1d61654db8f1740e6932a7dbd1
         ContentValues values = new ContentValues();
         values.put("USER_ID",user_id);
         values.put("MED_NAME",med_name);
@@ -148,23 +85,11 @@ public class MedicalDB extends SQLiteOpenHelper {
         values.put("DAYS",days);
         values.put("ENABLE",false);
         db.insert("MEDICINE",null,values);
-<<<<<<< HEAD
-        db.close();
-    }
-
-    public void deleteMedicine(SQLiteDatabase db, @NonNull int med_id) {
-        db = getWritableDatabase();
-        db.delete("MEDICINE", "_id=?", new String[]{String.valueOf(med_id)});
-        db.close();
-    }
-
-=======
     }
 
     public void deleteMedicine(SQLiteDatabase db, @NonNull int med_id){
         db.delete("MEDICINE","_id=?",new String[]{""+med_id});
     }
->>>>>>> e1554933036cee1d61654db8f1740e6932a7dbd1
     public void updateMedicine(SQLiteDatabase db,@NonNull int med_id, @NonNull String med_name, @NonNull int quantity, @NonNull String date_time, @NonNull String days){
         ContentValues values = new ContentValues();
         values.put("MED_NAME",med_name);
