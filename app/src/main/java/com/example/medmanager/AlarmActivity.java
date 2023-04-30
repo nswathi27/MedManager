879ca248833f1e0314f8a369f436a279cbbfcee7
Swathi_Nuli_S555089 <88883122+nswathi27@users.noreply.github.com>
2023-04-29 23:53:23 -0500

    Update AlarmActivity.java

--- app/src/main/java/com/example/medmanager/AlarmActivity.java
+++ app/src/main/java/com/example/medmanager/AlarmActivity.java
@@ -1,9 +1,11 @@
-<<<<<<< HEAD
-package com.example.medmanager;
+package com.Project.medmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -37,6 +39,13 @@ public class AlarmActivity extends AppCompatActivity {
     tookMed.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
+
+        MediaPlayer mediaPlayer;
+        String soundUriString = getIntent().getStringExtra("soundUri");
+        Uri soundUri = Uri.parse(soundUriString);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), soundUri);
+        mediaPlayer.stop();
+
         finish();
       }
     });
@@ -44,6 +53,11 @@ public class AlarmActivity extends AppCompatActivity {
     snooze.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
+        MediaPlayer mediaPlayer;
+        String soundUriString = getIntent().getStringExtra("soundUri");
+        Uri soundUri = Uri.parse(soundUriString);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), soundUri);
+        mediaPlayer.stop();
         Toast.makeText(getApplicationContext(),"Reminder set after 10 minutes.",Toast.LENGTH_LONG).show();
         finish();
       }
@@ -51,58 +65,4 @@ public class AlarmActivity extends AppCompatActivity {
 
 
   }
-=======
-package com.example.medmanager;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-public class AlarmActivity extends AppCompatActivity {
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_alarm);
-
-    TextView userName, medName, medTime, medQty;
-    Button tookMed, snooze;
-
-    userName = findViewById(R.id.alarm_user_name);
-    medName = findViewById(R.id.alarm_med_name);
-    medTime = findViewById(R.id.alarm_med_time);
-    medQty = findViewById(R.id.alarm_med_quantity);
-
-    tookMed = findViewById(R.id.alarm_took);
-    snooze = findViewById(R.id.alarm_snooze);
-
-    Intent intent = getIntent();
-    medName.setText(intent.getStringExtra("medName"));
-    medTime.setText("Time: "+intent.getStringExtra("medTime"));
-    medQty.setText("Qty: "+intent.getStringExtra("medQty"));
-    userName.setText(intent.getStringExtra("userName"));
-
-    tookMed.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        finish();
-      }
-    });
-
-    snooze.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Toast.makeText(getApplicationContext(),"Reminder set after 10 minutes.",Toast.LENGTH_LONG).show();
-        finish();
-      }
-    });
-
-
-  }
->>>>>>> e1554933036cee1d61654db8f1740e6932a7dbd1
 }
